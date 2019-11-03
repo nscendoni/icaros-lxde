5aded850c9235e419b0ca46bf36b91586d20e1c8
Nicola Scendoni <scendoni@gmail.com>
2019-11-03 15:20:36 +0100

    Fixes on iconify script

--- icaros-light_2.2.9-1/usr/lib/icaros/S/iconify.sh
+++ icaros-light_2.2.9-1/usr/lib/icaros/S/iconify.sh
@@ -3,13 +3,22 @@
 echo begin log>/tmp/log
 
 # Configuration
-desktop=/usr/lib/icaros/MyWorkspace/Desktop
-MyWorkspace=/usr/lib/icaros/MyWorkspace
+desktop=$HOME/Desktop
+MyWorkspace=$HOME
 
 # Iconify Event - Get informations
 active_window=`xdotool getactivewindow`
-pid=`xdotool getwindowpid $active_window=`
+pid=`xdotool getwindowpid $active_window`
 process_name=`ps -q $pid |grep $pid |awk '{ print $4 }'`
+
+# Check if the window is already iconified
+grep "PID_ICON=$pid" $desktop/* >/dev/null
+if [ $? = 0 ]
+then
+	echo "already iconified" >>log
+	exit
+fi
+
 
 # Search the icon name in .desktop files
 desktop_file=`grep -l $process_name /usr/share/applications/*.desktop|head -1`
@@ -19,19 +28,16 @@ icon_name=`/usr/lib/icaros/S/find_icon.py $desktop_icon_name`
 echo "desktop_file=$desktop_file" >>/tmp/log
 echo "icon_name=$icon_name" >>/tmp/log
 
-# icon_name=`/usr/lib/icaros/S/find_icon.py $process_name`
-
 # If icon is already present, append -num to icon
 if [[ -e $desktop/$process_name ]] ; then
     i=0
-	    while [[ -e $desktop/$process_name-$i ]] ; do
+    while [[ -e $desktop/$process_name-$i ]] ; do
         let i++
     done
     process_name=$process_name-$i
 fi
 
 window_name=`xdotool getwindowname $active_window`
-#todo: convert file, resize it and manage errors
 
 # Prepare Icaros Script
 file_name=$desktop/$process_name
@@ -39,9 +45,8 @@ cat >$file_name <<EOF
 echo "xdotool windowactivate $active_window">Home:.icarosd/runme
 delete MyWorkspace:Desktop/`basename $file_name` >NIL:
 delete MyWorkspace:Desktop/`basename $file_name`.info >NIL:
-; wait 1
-; echo "xdotool mousemove 800 10 click --repeat 3 1 key F5">Home:.icarosd/runme
 sh -c "touch MyWorkspace:.refresh"
+; PID_ICON=$pid
 EOF
 chmod +t $file_name
 chmod a+x $file_name
@@ -54,7 +59,6 @@ base_icon_name=`basename $icon_name`
 cd $desktop
 convert $base_icon_name -format png -resize 48x48\> $base_icon_name
 echo convert $desktop/$base_icon_name -format png -resize 48x48\> $desktop/$base_icon_name >>/tmp/log
-# icon_name_amiga=`basename ${process_name}`.info
 
 mv $desktop/`basename $icon_name` $desktop/`basename ${process_name}`.info
 echo mv $desktop/`basname $icon_name` $desktop/`basename ${process_name}`.info >>/tmp/log
@@ -64,6 +68,7 @@ echo cp $icon_name $desktop/$icon_name_amiga  >>/tmp/log
 #xdotool mousemove 800 10 click --repeat 3 1 key F5
 touch $MyWorkspace/.refresh
 
+# log
 echo $active_window >>/tmp/log
 echo $pid>>/tmp/log 
 echo $process_name>>/tmp/log
