5c56f86250ddf068ccc691e9b7ddc06678669b9e
Nicola Scendoni <scendoni@gmail.com>
2019-11-20 22:06:14 +0100

    fix on packages structure

--- icaros-lxde-light_0.1-1/usr/lib/icaros/S/sync_app.sh
+++ icaros-lxde-light_0.1-1/usr/lib/icaros/S/sync_app.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 destination_folder=/usr/lib/icaros/Linux
 
@@ -6,19 +6,16 @@ application_list=`find /usr/share/applications/ -name *.desktop`
 for application in $application_list
 do
 	echo $application
-	application_name=`grep -e "^Name=" $application |head -1| sed s/Name=//`
-	application_exe=`grep -e "^Exec=" $application | head -1 | sed s/Exec=//`
-	icon_name=`grep -e "^Icon=" $application | head -1 | sed s/Icon=//`
+	application_name=`grep -e "^Name=" $application | sed s/Name=//`
+	application_exe=`grep -e "^Exec=" $application | sed s/Exec=//`
+	icon_name=`grep -e "^Icon=" $application | sed s/Icon=//`
 	icon_file=`/usr/lib/icaros/S/find_icon.py $icon_name`
 	convert $icon_file -format png -resize 48x48\> "$destination_folder/$application_name".png
 	mv "$destination_folder/$application_name".png "$destination_folder/$application_name".info
 	
-	cp /usr/lib/icaros/System/Hosted/hbtemplate "$destination_folder/$application_name"
-	sed -i s/_COMMAND_/$(basename $application_exe)/ "$destination_folder/$application_name"
-	# absolute_path=$(basename $application_exe)
-	fullpath=`which $absolute_path`
-	sed -i s@_FULLPATH_@$fullpath@ "$destination_folder/$application_name"
-	parameters=`echo $application_exe|sed s/.*\ //| sed s/%.//`
-	sed -i s/_PARAMETERS_/$parameters/ "$destination_folder/$application_name"
+	cat >"$destination_folder/$application_name" <<EOF
+echo "$application_exe" >Home:.icarosd/runme
+EOF
 	chmod a+rxt "$destination_folder/$application_name"
+
 done
