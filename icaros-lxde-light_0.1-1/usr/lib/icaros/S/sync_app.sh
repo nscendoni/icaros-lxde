feb68f4ceb58db633a1f7c99c9f2b9a12630c171
Nicola Scendoni <scendoni@gmail.com>
2019-11-21 22:20:07 +0100

    Fixes on scripts and structure

--- icaros-lxde-light_0.1-1/usr/lib/icaros/S/sync_app.sh
+++ icaros-lxde-light_0.1-1/usr/lib/icaros/S/sync_app.sh
@@ -7,12 +7,17 @@ for application in $application_list
 do
 	echo $application
 	application_name=`grep -e "^Name=" $application | sed s/Name=//`
-	application_exe=`grep -e "^Exec=" $application | sed s/Exec=//`
-	icon_name=`grep -e "^Icon=" $application | sed s/Icon=//`
+	application_exe=`grep -e "^Exec=" $application | head -1 |sed s/Exec=//`
+	icon_name=`grep -e "^Icon=" $application |head -1 | sed s/Icon=//`
 	icon_file=`/usr/lib/icaros/S/find_icon.py $icon_name`
 	convert $icon_file -format png -resize 48x48\> "$destination_folder/$application_name".png
 	mv "$destination_folder/$application_name".png "$destination_folder/$application_name".info
 	
+	cp /usr/lib/icaros/System/Hosted/hbtemplate "$destination_folder/$application_name" 
+	sed -i s/_COMMAND_// "$destination_folder/$application_name"
+	sed -i s/_FULLPATH_/$application_exe/ "$destination_folder/$application_name"
+
+
 	cat >"$destination_folder/$application_name" <<EOF
 echo "$application_exe" >Home:.icarosd/runme
 EOF
